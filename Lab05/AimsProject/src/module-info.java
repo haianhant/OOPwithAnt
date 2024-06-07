248029894c603d0849176c838cc7111a4d234c08
Hải Anh Nguyễn Hữu Hoàng <hunganhz179@gmail.com>
2024-06-08 00:00:59 +0700

    update lab05

--- Lab05/AimsProject/src/module-info.java
+++ Lab05/AimsProject/src/module-info.java
@@ -5,4 +5,12 @@ module AimsProject {
     requires java.base;
     requires java.desktop;
     
+    
+    exports hust.soict.globalict.aims.media;
+    exports hust.soict.globalict.aims.store;
+    exports hust.soict.globalict.aims.screen.customer.controller;
+    exports hust.soict.globalict.aims.exception;
+    exports hust.soict.globalict.test.screen.customer.store to javafx.graphics;
+    
+    opens hust.soict.globalict.aims.screen.customer.controller to javafx.fxml;
 }
